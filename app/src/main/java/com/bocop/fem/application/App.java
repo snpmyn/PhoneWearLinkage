8bb71ffb3c29e0584d89d263eef2c9f4c7bef1e4
snpmyn <snpmyn@126.com>
2021-09-06 14:45:42 +0800

    升级

--- app/src/main/java/com/bocop/fem/application/App.java
+++ app/src/main/java/com/bocop/fem/application/App.java
@@ -1,15 +1,11 @@
 package com.bocop.fem.application;
 
-import android.app.Activity;
-import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 
 import com.bocop.fem.BuildConfig;
 import com.bocop.fem.value.Folder;
-import com.chaos.basic.BasicApp;
 import com.chaos.litepal.configure.LitePalInitConfigure;
-import com.chaos.util.java.activity.ActivitySuperviseManager;
+import com.chaos.litepool.application.LitePoolApp;
 import com.chaos.util.java.storage.mmkv.MmkvInitConfigure;
 import com.chaos.widget.crash.CrashManagerInitConfigure;
 import com.tencent.mmkv.MMKVContentChangeNotification;
@@ -17,18 +13,16 @@ import com.tencent.mmkv.MMKVHandler;
 import com.tencent.mmkv.MMKVLogLevel;
 import com.tencent.mmkv.MMKVRecoverStrategic;
 
-import org.jetbrains.annotations.NotNull;
-
 import configure.FragmentationInitConfig;
 import timber.log.Timber;
 
 /**
  * Created on 2021/8/13
  *
- * @author zsp
+ * @author zspx
  * @desc 应用
  */
-public class App extends BasicApp implements MMKVHandler, MMKVContentChangeNotification {
+public class App extends LitePoolApp implements MMKVHandler, MMKVContentChangeNotification {
     /**
      * 应用程序创调
      * <p>
@@ -46,8 +40,6 @@ public class App extends BasicApp implements MMKVHandler, MMKVContentChangeNotif
      * 初始化配置
      */
     private void initConfiguration() {
-        // 全局监听 Activity 生命周期
-        registerActivityListener();
         // MMKV
         MmkvInitConfigure.initMmkv(this, BuildConfig.DEBUG, this, this);
         // 崩溃管理器
@@ -58,62 +50,18 @@ public class App extends BasicApp implements MMKVHandler, MMKVContentChangeNotif
         FragmentationInitConfig.initFragmentation(BuildConfig.DEBUG);
     }
 
-    /**
-     * Activity 全局监听
-     */
-    private void registerActivityListener() {
-        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
-            @Override
-            public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
-                // 添监听到创事件 Activity 至集合
-                ActivitySuperviseManager.getInstance().pushActivity(activity);
-            }
-
-            @Override
-            public void onActivityStarted(@NonNull Activity activity) {
-
-            }
-
-            @Override
-            public void onActivityResumed(@NonNull Activity activity) {
-
-            }
-
-            @Override
-            public void onActivityPaused(@NonNull Activity activity) {
-
-            }
-
-            @Override
-            public void onActivityStopped(@NonNull Activity activity) {
-
-            }
-
-            @Override
-            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
-
-            }
-
-            @Override
-            public void onActivityDestroyed(@NonNull Activity activity) {
-                // 移监听到销事件 Activity 出集合
-                ActivitySuperviseManager.getInstance().removeActivity(activity);
-            }
-        });
+    @Override
+    public void onContentChangedByOuterProcess(String mmapID) {
+        Timber.i("[content changed] %s", mmapID);
     }
 
     @Override
-    public void onContentChangedByOuterProcess(String s) {
-        Timber.i("[content changed] %s", s);
-    }
-
-    @Override
-    public MMKVRecoverStrategic onMMKVCRCCheckFail(String s) {
+    public MMKVRecoverStrategic onMMKVCRCCheckFail(String mmapID) {
         return MMKVRecoverStrategic.OnErrorRecover;
     }
 
     @Override
-    public MMKVRecoverStrategic onMMKVFileLengthError(String s) {
+    public MMKVRecoverStrategic onMMKVFileLengthError(String mmapID) {
         return MMKVRecoverStrategic.OnErrorRecover;
     }
 
@@ -123,9 +71,9 @@ public class App extends BasicApp implements MMKVHandler, MMKVContentChangeNotif
     }
 
     @Override
-    public void mmkvLog(@NotNull MMKVLogLevel mmkvLogLevel, String s, int i, String s1, String s2) {
-        String log = ("< " + s + " : " + i + " :: " + s1 + " > " + s2);
-        switch (mmkvLogLevel) {
+    public void mmkvLog(@NonNull MMKVLogLevel level, String file, int line, String function, String message) {
+        String log = ("< " + file + " : " + line + " :: " + function + " > " + message);
+        switch (level) {
             case LevelDebug:
                 Timber.d("[redirect logging MMKV] %s", log);
                 break;
